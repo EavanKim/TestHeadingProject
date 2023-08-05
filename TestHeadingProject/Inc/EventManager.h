6663907ba20e8fb28f64ec09407135467102964d
eavankim <kjtkjt21@gmail.com>
2023-08-06 05:21:44 +0900

    헤더 이넘 정비

--- TestHeadingProject/Inc/EventManager.h
+++ TestHeadingProject/Inc/EventManager.h
@@ -1,14 +1,34 @@
 #pragma once
 
+
+//typedef SendStruct<1, 1> PCK_SessionKey;
+//typedef SendStruct<2, 1> PCK_Shutdown;
+//typedef SendStruct<3, 1> PCK_Ping;
+//typedef SendStruct<4, 1> PCK_Pong;
+//typedef SendStruct<10000, 12> PCK_CS_Enter;
+//typedef SendStruct<10001, 2> PCK_CS_Exit;
+//typedef SendStruct<10002, 100> PCK_CS_Chatting;
+//typedef SendStruct<10003, 112> PCK_CS_Wispering;
+//typedef SendStruct<10004, 112> PCK_SC_Wispering;
+//typedef SendStruct<10005, 8> PCK_CS_RequestPrevious;
+//typedef SendStruct<10006, 8> PCK_SC_ReturnEnter;
+//typedef SendStruct<10007, 120> PCK_SC_OthersChatting;
+//typedef SendStruct<10008, 8> PCK_SC_RequestReset;
 enum E_PCK_TYPE
 {
+	PCK_SessionKey = 1,
+	PCK_Shutdown,
+	PCK_Ping,
+	PCK_Pong,
 	PCK_CS_ENTER = 10000,
 	PCK_CS_EXIT,
 	PCK_CS_CHATTING,
 	PCK_CS_WISPERING,
+	PCK_SC_WISPERING,
 	PCK_CS_REQUESTPREVIOUS,
 	PCK_SC_RETURNENTER,
 	PCK_SC_OTHERSCHATTING,
+	PCK_SC_REQUESTRESET,
 	PCK_CS_MAX
 };
 
